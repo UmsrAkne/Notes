59b8741b6e3435fe9ac7b1e9679083b05f48968e
Akn <sibata486@gmail.com>
2024-09-22 02:49:39 +0900

    add feat / ファイルのドラッグアンドドロップ機能を実装。

---
+++ Notes/Behaviors/DragAndDropBehavior.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using Microsoft.Xaml.Behaviors;
+using Notes.ViewModels;
+
+namespace Notes.Behaviors
+{
+    public class DragAndDropBehavior : Behavior<Window>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            // ファイルをドラッグしてきて、コントロール上に乗せた際の処理
+            AssociatedObject.PreviewDragOver += AssociatedObject_PreviewDragOver;
+
+            // ファイルをドロップした際の処理
+            AssociatedObject.Drop += AssociatedObject_Drop;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.PreviewDragOver -= AssociatedObject_PreviewDragOver;
+            AssociatedObject.Drop -= AssociatedObject_Drop;
+        }
+
+        private void AssociatedObject_Drop(object sender, DragEventArgs e)
+        {
+            // ファイルパスの一覧の配列
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (((Window)sender).DataContext is not MainWindowViewModel vm)
+            {
+                return;
+            }
+
+            if (files == null)
+            {
+                return;
+            }
+
+            foreach (var f in files)
+            {
+                vm.ScrapContainer.Add(f);
+            }
+        }
+
+        private void AssociatedObject_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Copy;
+            e.Handled = e.Data.GetDataPresent(DataFormats.FileDrop);
+        }
+    }
+}
--- Notes/Models/ScrapContainer.cs
+++ Notes/Models/ScrapContainer.cs
@@ -15,5 +15,15 @@ namespace Notes.Models
         public ObservableCollection<Scrap> Scraps { get; private set; } = new ();
 
         public CursorManager CursorManager { get; set; }
+
+        /// <summary>
+        /// 入力された文字から Scrap オブジェクトを生成して、 Scraps に追加します。
+        /// </summary>
+        /// <param name="str">ファイルパスであれば、そのファイルの種類に応じたものが、それ以外ならば単純なテキストの Scrap が追加されます。</param>
+        public void Add(string str)
+        {
+            // Todo : まだ完全に実装していない。
+            Scraps.Add(new Scrap() { Title = str, });
+        }
     }
 }
