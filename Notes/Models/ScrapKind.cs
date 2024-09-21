06d5a81a7c5474b160a9e09eaaee350cda724ae5
Akn <sibata486@gmail.com>
2024-09-22 02:02:41 +0900

    add / Scrap.cs, ScrapKind.cs を追加。

---
+++ Notes/Models/ScrapKind.cs
@@ -0,0 +1,30 @@
+﻿namespace Notes.Models
+{
+    public enum ScrapKind
+    {
+        /// <summary>
+        /// スクラップの内容がファイルパス以外のテキストであることを表します。
+        /// </summary>
+        Text,
+
+        /// <summary>
+        /// スクラップの内容がファイルパスであることを表します。
+        /// </summary>
+        FilePath,
+
+        /// <summary>
+        /// スクラップの内容が音声ファイルであることを表します。
+        /// </summary>
+        Sound,
+
+        /// <summary>
+        /// スクラップの内容が画像ファイルであることを表します。
+        /// </summary>
+        Image,
+
+        /// <summary>
+        /// スクラップの内容が実行ファイルであることを表します。
+        /// </summary>
+        Executable,
+    }
+}
