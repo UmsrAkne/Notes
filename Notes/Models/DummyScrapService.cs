61f2bb682b862a58b9412970713df505491bbdd7
Akn <sibata486@gmail.com>
2024-09-22 16:31:10 +0900

    add / アプリ起動時にダミーのデータを挿入。

---
+++ Notes/Models/DummyScrapService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Notes.Models
+{
+    public class DummyScrapService : IScrapService
+    {
+        public IEnumerable<Scrap> GetScraps()
+        {
+            var l = new List<Scrap>();
+            for (var i = 0; i < 30; i++)
+            {
+                l.Add(new Scrap
+                {
+                    IndentCount = 0,
+                    Title = $"testTitle{i + 1}",
+                    Description = $"testDescription{i + 1}",
+                    IsMarked = false,
+                    Kind = ScrapKind.Text,
+                });
+            }
+
+            return l;
+        }
+    }
+}
