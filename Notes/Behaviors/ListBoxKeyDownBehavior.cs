d91563f5c8b418a100447221c636c7b4ae557817
Akn <sibata486@gmail.com>
2024-09-30 00:01:35 +0900

    add feat / Ctrl + U, Ctrl + D に、ページアップ・ページダウン機能を割り当て。

--- Notes/Behaviors/ListBoxKeyDownBehavior.cs
+++ Notes/Behaviors/ListBoxKeyDownBehavior.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using Microsoft.Xaml.Behaviors;
 using Notes.Models;
 using Notes.ViewModels;
@@ -9,6 +12,8 @@ namespace Notes.Behaviors
 {
     public class ListBoxKeyDownBehavior : Behavior<ListBox>
     {
+        private ScrollViewer scrollViewer;
+
         protected override void OnAttached()
         {
             base.OnAttached();
@@ -113,12 +118,72 @@ namespace Notes.Behaviors
                     }
 
                     break;
+
+                case Key.D:
+                    if (isControlPressed && listBox.SelectedIndex < listBox.Items.Count - 1)
+                    {
+                        MoveToPage(listBox, true);
+                    }
+
+                    break;
+
+                case Key.U:
+                    if (isControlPressed && listBox.SelectedIndex > 0)
+                    {
+                        MoveToPage(listBox, false);
+                    }
+
+                    break;
             }
 
             if (listBox.SelectedItem != null)
             {
                 listBox.ScrollIntoView(listBox.SelectedItem);
             }
+        }
+
+        private ScrollViewer GetScrollViewer(DependencyObject o)
+        {
+            if (o is ScrollViewer viewer)
+            {
+                return viewer;
+            }
+
+            for (var i = 0; i < VisualTreeHelper.GetChildrenCount(o); i++)
+            {
+                var child = VisualTreeHelper.GetChild(o, i);
+                var result = GetScrollViewer(child);
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+
+            return null;
+        }
+
+        private void MoveToPage(ListBox listBox, bool isNext)
+        {
+            scrollViewer ??= GetScrollViewer(listBox);
+
+            var moveCountPerPage = 0;
+            if (listBox.Items.Count > 0)
+            {
+                var item = listBox.Items[0];
+                if (item != null)
+                {
+                    listBox.ScrollIntoView(item);
+                    if (listBox.ItemContainerGenerator.ContainerFromIndex(0) is FrameworkElement container)
+                    {
+                        moveCountPerPage = (int)(scrollViewer.ActualHeight / container.ActualHeight) - 1;
+                    }
+                }
+            }
+
+            var newIndex = listBox.SelectedIndex;
+            newIndex += isNext ? moveCountPerPage : -moveCountPerPage;
+            newIndex = Math.Max(0, Math.Min(newIndex, listBox.Items.Count - 1));
+            listBox.SelectedIndex = newIndex;
         }
     }
 }
